20576c0b8bc252195274aeb66a2e965a0b5d27d2
IvyZheng85 <ivy.zheng85@gmail.com>
2021-04-28 09:51:00 -0700

    add some content

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
 
