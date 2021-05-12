15138c3e18959e4cd44ac710ea9e7d470f2f5b13
Timothy Nguyen <nguyeti4@flip1.engr.oregonstate.edu>
2021-05-12 11:32:39 -0700

    update data_manipulation

--- SQL/data_manipulation.sql
+++ SQL/data_manipulation.sql
@@ -135,66 +135,31 @@ where question_id = :id_from_update and choice_id = :choice_id_from_update;
 Insert into Simulators (user_id, grade, play_date, scenario)
 Values (:IDInput, :gradeInput, :dateInput, :sceneInput);
 
---Lookup Simulator record
+--Lookup Simulator record by scenario name
 Select * from Simulators
-where result_id = :resultInput OR user_id = :userInput OR grading = :gradingInput OR play_date = :playInput OR Scenario = :scenInput;
+where Scenario = :scenInput;
 
---Lokokup all Simulator records if none of input fields are filled
+--Lookup Simulator records you want to delete
 Select * from Simulators
+where playdate = :play_from_update;
 
 --Delete the Searched Record
-Delete * from Users 
-where result_id = :result_from_update
+Delete * from Simulators 
+where playdate < :play_from_update
 
 --QuizRecords Page
 --add a new Record
 Insert into QuizRecords (user_id, quiz_date, quiz_state, quiz_score)
 Values (:useInput, :quizInput, :stateInput, :scoreInput);
 
---Lookup Quiz record
-Select * from QuizRecords
-where quiz_id = :qInput OR user_id = :uInput OR grading = :gInput OR play_date = :pInput OR Scenario = :sInput;
+--Lookup all Quiz records by state
+Select * from QuizRecords where quiz_state = :State_input
 
---Lookup all Quiz records if none of input fields are filled
-Select * from QuizRecords
+--Lookup all Quiz records by date
+Select * from QuizRecords where quiz_date < :quiz_delete_input
 
---Delete the Searched Record
-Delete * from Users 
-where quiz_id = :quiz_from_update
+--Delete the Searched records that are older than specified date
+Delete * from QuizRecords
+where quiz_date < :quiz_delete_input
 
 
--- get all Planet IDs and Names to populate the Homeworld dropdown
-SELECT planet_id, name FROM bsg_planets
-
--- -- get all characters and their homeworld name for the List People page
--- SELECT bsg_people.character_id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id
-
--- -- get a single character's data for the Update People form
--- SELECT character_id, fname, lname, homeworld, age FROM bsg_people WHERE character_id = :character_ID_selected_from_browse_character_page
-
--- -- get all character's data to populate a dropdown for associating with a certificate  
--- SELECT character_id AS pid, fname, lname FROm bsg_people 
--- -- get all certificates to populate a dropdown for associating with people
--- SELECT certification_id AS cid, title FROM bsg_cert
-
--- -- get all peoople with their current associated certificates to list
--- SELECT pid, cid, CONCAT(fname,' ',lname) AS name, title AS certificate 
--- FROM bsg_people 
--- INNER JOIN bsg_cert_people ON bsg_people.character_id = bsg_cert_people.pid 
--- INNER JOIN bsg_cert on bsg_cert.certification_id = bsg_cert_people.cid 
--- ORDER BY name, certificate
-
--- -- add a new character
--- INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES (:fnameInput, :lnameInput, :homeworld_id_from_dropdown_Input, :ageInput)
-
--- -- associate a character with a certificate (M-to-M relationship addition)
--- INSERT INTO bsg_cert_people (pid, cid) VALUES (:character_id_from_dropdown_Input, :certification_id_from_dropdown_Input)
-
--- -- update a character's data based on submission of the Update Character form 
--- UPDATE bsg_people SET fname = :fnameInput, lname= :lnameInput, homeworld = :homeworld_id_from_dropdown_Input, age= :ageInput WHERE id= :character_ID_from_the_update_form
-
--- -- delete a character
--- DELETE FROM bsg_people WHERE id = :character_ID_selected_from_browse_character_page
-
--- -- dis-associate a certificate from a person (M-to-M relationship deletion)
--- DELETE FROM bsg_cert_people WHERE pid = :character_ID_selected_from_certificate_and_character_list AND cid = :certification_ID_selected_from-certificate_and_character_list
