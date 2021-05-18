9eb0feda21534bb4e9bcd8a4e190e71a5a8c0dac
nguyeti4 <72110231+nguyeti4@users.noreply.github.com>
2021-05-17 21:55:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,20 @@ def users():
 
 @app.route("/sim_user",methods=["POST","GET"])
 def sim_user():
-    return render_template("simulators.html")
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        return render_template("simulators.html")
+    elif request.method == 'POST':
+        print("Add new simulator record!")
+        user_id = request.form['user_id']
+        grade = request.form['grade']
+        date = request.form['play_date']
+        scenario = request.form['scenario']
+
+        query = 'INSERT INTO Simulators (user_id, grading, play_date, scenario_name) VALUES (%s,%s,%s,%s)'
+        data = (user_id, grade, date, scenario)
+        execute_query(db_connection, query, data)
+        return ('sim record added!')
 
 @app.route("/quiz_user", methods=["POST", "GET"])
 def quiz_user():
