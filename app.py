49bb454bba5a42266ff769b1015bcbdde10b3fb4
IvyZheng85 <ivy.zheng85@gmail.com>
2021-05-18 10:09:22 -0700

    add post in users page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify
 from datetime import timedelta
 from db_connector import connect_to_database, execute_query
 
@@ -12,7 +12,22 @@ def users():
     # db_connection = connect_to_database()
     # query = "SELECT fname, lname, homeworld, age, id from bsg_people;"
     # result = execute_query(db_connection, query).fetchall()
-    return render_template("users.html")
+    if request.method == 'GET':
+        return render_template("users.html")
+    elif request.method == 'POST':
+        name = request.form['user_name']
+        password = request.form['user_password']
+        email = request.form['user_email']
+        regis_date = request.form['regis_date']
+        db_connection = connect_to_database()
+        query = '''Insert into Users (user_name, user_password, user_email, regis_date) Values (%s,%s,%s,%s)'''
+        query_params = (name, password, email, regis_date)
+        execute_query(db_connection, query, query_params)
+        query = "SELECT * from Users where user_id = (select max(user_id) from Users);"
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+        return render_template("users.html")
+        
 
 @app.route("/sim_user",methods=["POST","GET"])
 def sim_user():
@@ -33,4 +48,5 @@ def questions():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host='localhost', port=5000)
