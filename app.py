23914f2bf4f81da4fb8b756ff0d0381d540d9949
nguyeti4 <72110231+nguyeti4@users.noreply.github.com>
2021-05-27 00:40:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -227,6 +227,9 @@ def quiz_user():
         query = 'Select * from Users where user_id=%s'
         user = execute_query(db_connection, query, (input_id,)).fetchone()
         print(user)
+        if input_id == '':
+            flash("Please enter a user id!")
+            return redirect(url_for("quiz_records_page"))
         if user is None:
             #print('This user does not exist!')
             flash("This user does not exist!")
